77f76ab81898388ef955a1854838ac76fcd5824b
Weltenfeind <121118273+Weltenfeind@users.noreply.github.com>
2022-12-26 19:25:19 +0100

    Add files via upload

---
+++ ejercicio_strings_booleans_type_int.py
@@ -0,0 +1,22 @@
+# string es una secuencia de texto:
+mi_nombre = 'Alex Fengkehli Brito Martinez'
+print( mi_nombre)
+
+#Funcion nueva: Type
+# Type te deja saber que tipo de variable es el dato, en el caso de los strings (str) seria: 
+print(mi_nombre)
+print(type(mi_nombre))
+
+#int: numeros enteros
+mi_edad = 28
+print('Mi edad es', mi_edad)
+print(type(mi_edad))
+
+#boolean: verdadero o falso, representado por True o False
+#importante destacar la mayuscula siempre "True" o "False"
+#nunca "true" o "false", ejemplo:
+
+es_soltero = True
+print('es soltero =', es_soltero)
+print(type(es_soltero))
+
